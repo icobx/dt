d3b0b9a503c5c8fb5c2ddce0c26f3e2278b207ca
Jakub Fedorko <jakub.fedorko@icloud.com>
2022-03-25 09:19:18 +0100

    add attention mechanism to bi-lstm

--- bi_lstm.py
+++ bi_lstm.py
@@ -1,9 +1,71 @@
+from zmq import device
 from bert_embedding_model import BertEmbeddingModel
 import torch
 import torch.nn as nn
 import numpy as np
 
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+
+
+class Attention(nn.Module):
+
+    def __init__(self, hidden_dim, device):
+        super(Attention, self).__init__()
+
+        self.hidden_dim = hidden_dim
+        self.device = device
+        self.attention_weights = nn.Parameter(torch.Tensor(1, hidden_dim), requires_grad=True)
+        self.relu = nn.ReLU()
+
+        stdv = 1.0 / np.sqrt(self.hidden_dim)
+        for weight in self.attention_weights:
+            nn.init.uniform_(weight, -stdv, stdv)
+
+    def get_mask(self):
+        pass
+
+    def forward(self, inputs, lengths):
+        batch_size, max_len = inputs.size()[:2]
+
+        # swap dimensions (permute), add unitary dimension at the start (unsqueeze), repeat the the tensor (repeaat)
+        weights = self.attention_weights.permute(1, 0).unsqueeze(0).repeat(batch_size, 1, 1)
+        # print('pre bmm: ', weights.size())
+        # print('pre bmm: ', weights)
+        # apply attention layer
+        weights = torch.bmm(
+            inputs,
+            weights
+        )
+        # print('weights', weights.size())
+        # print('weights', weights)
+        attentions = torch.softmax(self.relu(weights.squeeze()), dim=-1)
+        # print('attentions', attentions.size())
+        # print('attentions', attentions)
+
+        mask = torch.ones(attentions.size(), requires_grad=True).to(self.device)
+        with torch.no_grad():
+            for i, l in enumerate(lengths):
+                if l < max_len:
+                    mask[i, l:] = 0
+
+        masked = attentions * mask
+        # print('masked', masked.size())
+        # print('masked', masked)
+        _sums = masked.sum(-1).unsqueeze(-1)  # sums per row
+        # print('_sums', _sums.size())
+        # print('_sums', _sums)
+        attentions = masked.div(_sums)
+        # print('attentions after masked:', attentions.size())
+        # print('attentions after masked:', attentions)
+        # apply attention weights
+        weighted = torch.mul(inputs, attentions.unsqueeze(-1).expand_as(inputs))
+        # print('weighted', weighted.size())
+        # print('weighted', weighted)
+        # get the final fixed vector representations of the sentences
+        # representations = weighted.sum(1).squeeze()
+        # print('representations', representations.size())
+        # print('representations', representations)
+        return weighted, attentions
 
 
 class BiLSTM(nn.Module):
@@ -14,7 +76,8 @@ class BiLSTM(nn.Module):
         lstm_dropout=0,
         hidden_dim=128,
         embedding_dim=768,
-        sent_level_feature_dim=0
+        sent_level_feature_dim=0,
+        device=torch.device('cpu')
     ):
         super(BiLSTM, self).__init__()
 
@@ -28,13 +91,15 @@ class BiLSTM(nn.Module):
             bidirectional=True,
             dropout=lstm_dropout
         )
+        self.attention = Attention(hidden_dim=2*hidden_dim, device=device)
         self.drop = nn.Dropout(p=dropout)
         self.dense = nn.Linear(2*hidden_dim+sent_level_feature_dim, 1)
 
     def forward(self, embeddings, lengths, sent_level_features=None):
         packed = pack_padded_sequence(embeddings, lengths, batch_first=True, enforce_sorted=False)
         packed_out, _ = self.lstm(packed)
-        output, _ = pad_packed_sequence(packed_out, batch_first=True)
+        output, lens = pad_packed_sequence(packed_out, batch_first=True)
+        output, _ = self.attention(output, lens)
 
         out_forward = output[range(len(output)), lengths - 1, :self.hidden_dim]
         out_reverse = output[:, 0, self.hidden_dim:]
@@ -76,4 +141,4 @@ class BiLSTM(nn.Module):
 # yy = bi(xx, xxx)
 
 # print(yy.size())
-# print(yy)
+# print(torch.sigmoid(yy))
